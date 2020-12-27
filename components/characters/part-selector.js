825aeb316741c00b472c1aea8d9d9d33796c1861
Norman Arredondo Martinez <arredondo_norman25@outlook.es>
2020-12-27 00:00:23 -0600

    Emit para comunicación de child a parent

---
+++ components/characters/part-selector.js
@@ -0,0 +1,61 @@
+const PartSelector = {
+    template:
+        `
+    <div class="body-part position-relative">
+        <div class="prev-area" @mouseover="hoverPrev = true" @mouseleave="hoverPrev = false">
+            <button class="prev-btn" v-show="hoverPrev" @click="selectPrevPart()"> < </button>
+        </div>
+        <img :src="selectedPart.src" class="body-img heads-img about-selected">
+        <div class="next-area" @mouseover="hoverNext = true" @mouseleave="hoverNext = false">
+                <button class="next-btn" v-show="hoverNext" @click="selectNextPart()"> > </button>
+        </div>
+    </div>
+    `,
+
+    props: {
+        parts:{
+            type: Array,
+            required: true,
+            
+        },
+        rand: {
+            type: Boolean
+        }
+    },
+    data() {
+        return {
+            hoverPrev: false,
+            hoverNext: false,
+
+            selectedPartIndex: 0,
+
+            minIndex: 0,
+            maxIndex:this.parts.length - 1
+        };
+    },
+    watch: {
+        rand: function(val) {
+            if(val == true) {
+                this.selectedPartIndex = Math.floor(Math.random() * (this.maxIndex - this.minIndex +1) )+ this.minIndex;
+                this.$emit('randomInvoked',true);
+            }
+            
+        }
+    },
+    computed: {
+        selectedPart() {
+            return this.parts[this.selectedPartIndex];
+        }
+    },
+    methods: {
+        selectPrevPart() {
+            this.selectedPartIndex = this.selectedPartIndex > this.minIndex ? this.selectedPartIndex - 1 : this.maxIndex;
+        },
+
+        selectNextPart() {
+            this.selectedPartIndex = this.selectedPartIndex < this.maxIndex ? this.selectedPartIndex + 1 : this.minIndex;
+
+        }
+    }
+
+}
--- components/characters/the-main-area.js
+++ components/characters/the-main-area.js
@@ -1,35 +1,59 @@
 const TheMainArea = {
-    template: 
-    `
-                <div class="container-fluid">
-                <h1 class="main-title"> Valar Morghulis.. </h1>
-                <h2 class="main-subtitle"> Who's next? </h2>
-                <div class="row">
+    template:
+        `
+    <div class="container-fluid">
+        <h1 class="main-title"> Valar Morghulis.. </h1>
+        <h2 class="main-subtitle"> Who's next? </h2>
+        <div class="row">
 
                     <!--Cabeza-->
                     <div class="col-12">
                         <div class="body-row head-row d-flex justify-content-center">
-                            <div class="body-part position-relative">
-                                <div class="prev-area" @mouseover="hoverPrev = true" @mouseleave="hoverPrev = false">
-                                    <button class="prev-btn" v-show="hoverPrev" @click="selectPrevHead()">
-                                        < </button>
-
-                                </div>
-                                <img :src="fileParts.heads[selectedHeadIndex].src" class="body-img heads-img about-selected">
-                                <div class="next-area" @mouseover="hoverNext = true" @mouseleave="hoverNext = false">
-                                    <button class="next-btn" v-show="hoverNext" @click="selectNextHead()"> > </button>
-                                </div>
-                            </div>
-
+                            <PartSelector :parts="fileParts.heads" 
+                                          :rand="randomHead"
+                                          @randomInvoked="invoked => randomHead = !invoked"></PartSelector>
                         </div>
-
                     </div>
 
                     <!--Cuerpo-->
 
                     <div class="col-12">
                         <div class="body-row head-row d-flex justify-content-center">
-                            <div class="body-part position-relative">
+                        <PartSelector :parts="fileParts.middles"
+                                      :rand="randomMiddle"
+                                      @randomInvoked="invoked => randomMiddle = !invoked"></PartSelector>
+
+                        </div>
+                    </div>
+
+
+                    <!--Pies-->
+
+                    <div class="col-12">
+                        <div class="body-row head-row d-flex justify-content-center">
+                        <PartSelector :parts="fileParts.foots"
+                                  :rand="randomFoot"
+                                  @randomInvoked="invoked => randomFoot = !invoked"></PartSelector>
+
+                        </div>
+                    </div>
+                
+
+                <div class="btn-area d-flex justify-content-center">
+                    <div class="btn-container d-flex justify-content-between">
+                        <button class="btn btn-light" @click="randomAll()"> Random </button>
+                        <button id="about-btn" class="btn btn-light"> About </button>
+                        <button class="btn btn-light"> Download </button>
+                    </div>
+                </div>
+
+        </div>
+    </div>
+    `,
+
+    /*
+    cuerpo
+    <div class="body-part position-relative">
                                 <div class="prev-area" @mouseover="hoverPrev = true" @mouseleave="hoverPrev = false">
                                     <button class="prev-btn" v-show="hoverPrev" @click="selectPrevBody()">
                                         < </button>
@@ -40,16 +64,13 @@ const TheMainArea = {
                                     <button class="next-btn" v-show="hoverNext" @click="selectNextBody()"> > </button>
                                 </div>
                             </div>
+    
+    */
 
-                        </div>
-                    </div>
+    /*
+    pies
 
-
-                    <!--Pies-->
-
-                    <div class="col-12">
-                    <div class="body-row head-row d-flex justify-content-center">
-                        <div class="body-part position-relative">
+    <div class="body-part position-relative">
                             <div class="prev-area" @mouseover="hoverPrev = true" @mouseleave="hoverPrev = false">
                                 <button class="prev-btn" v-show="hoverPrev" @click="selectPrevFoot()">
                                     < </button>
@@ -60,23 +81,16 @@ const TheMainArea = {
                                 <button class="next-btn" v-show="hoverNext" @click="selectNextFoot()"> > </button>
                             </div>
                         </div>
+    
+    */ 
 
-                    </div>
-                
-
-                <div class="btn-area d-flex justify-content-center">
-                    <div class="btn-container d-flex justify-content-between">
-                        <button class="btn btn-light"> Random </button>
-                        <button id="about-btn" class="btn btn-light"> About </button>
-                        <button class="btn btn-light"> Download </button>
-                    </div>
-                </div>
-            </div>
-    `,
+    components: { PartSelector },
 
     data() {
         return {
             fileParts: bodies,
+            
+            /*
             hoverPrev: false,
             hoverNext: false,
 
@@ -84,33 +98,52 @@ const TheMainArea = {
             selectedBodyIndex: 0,
             selectedFootIndex: 0,
 
-            minIndex:  0,
-            maxIndex: bodies.heads.length -1
+            minIndex: 0,
+            maxIndex: bodies.heads.length - 1
+            */
+
+            randomHead: false,
+            randomMiddle: false,
+            randomFoot: false,
+            selectedCharId: 1,
+            selectedParts: {
+                head: {},
+                middle: {},
+                foots: {}
+            }
 
         }
     },
-    methods:{
+    methods: {
+        /*
         selectPrevHead() {
-            this.selectedHeadIndex = this.selectedHeadIndex > this.minIndex ? this.selectedHeadIndex -1 : this.maxIndex;
+            this.selectedHeadIndex = this.selectedHeadIndex > this.minIndex ? this.selectedHeadIndex - 1 : this.maxIndex;
         },
         selectNextHead() {
-            this.selectedHeadIndex = this.selectedHeadIndex < this.maxIndex ? this.selectedHeadIndex +1 : this.minIndex;
+            this.selectedHeadIndex = this.selectedHeadIndex < this.maxIndex ? this.selectedHeadIndex + 1 : this.minIndex;
         },
 
         selectPrevBody() {
-            this.selectedBodyIndex = this.selectedBodyIndex > this.minIndex ? this.selectedBodyIndex -1 : this.maxIndex;
+            this.selectedBodyIndex = this.selectedBodyIndex > this.minIndex ? this.selectedBodyIndex - 1 : this.maxIndex;
         },
         selectNextBody() {
-            this.selectedBodyIndex = this.selectedBodyIndex < this.maxIndex ? this.selectedBodyIndex +1 : this.minIndex;
+            this.selectedBodyIndex = this.selectedBodyIndex < this.maxIndex ? this.selectedBodyIndex + 1 : this.minIndex;
         },
 
         selectPrevFoot() {
-            this.selectedFootIndex = this.selectedFootIndex > this.minIndex ? this.selectedFootIndex -1 : this.maxIndex;
+            this.selectedFootIndex = this.selectedFootIndex > this.minIndex ? this.selectedFootIndex - 1 : this.maxIndex;
         },
         selectNextFoot() {
-            this.selectedFootIndex = this.selectedFootIndex < this.maxIndex ? this.selectedFootIndex +1 : this.minIndex;
+            this.selectedFootIndex = this.selectedFootIndex < this.maxIndex ? this.selectedFootIndex + 1 : this.minIndex;
+        }
+        */
+
+        randomAll() {
+            this.randomHead = true;
+            this.randomMiddle = true;
+            this.randomFoot = true;
+
         }
 
-
-    }
+    },
 }
